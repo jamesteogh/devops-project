65807eaadbf26bd87041fb399d9fa4da318f0750
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-12-01 00:29:22 +0800

    feat: create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
